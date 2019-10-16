a6cb17b82f12755768ec49004aad12f5d416061c
hengloem <heng.loem2017@gmail.com>
2019-10-16 15:24:33 +0700

    Setup page gination

--- src/Model/static.datasource.ts
+++ src/Model/static.datasource.ts
@@ -5,9 +5,13 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class StaticDataSource {
     private products: Product[] = [
-        new Product(1, "Baby", "Cat01", "Fresh", 12000),
-        new Product(2, "T-shirt", "Cat02", "Beer", 2000),
-        new Product(3, "Trouser", "Cat02", "Beer", 2500)
+        new Product(1, "Baby", "Cat01", "plan, etc.", 1000),
+        new Product(2, "T-shirt", "Cat01", "Beer", 200),
+        new Product(3, "Trouser", "Cat01", "Beer", 200),
+        new Product(4, "Cupple", "Cat01", "Sand", 20),
+        new Product(5, "Sandwitch", "Cat01", "Sand", 50),
+        new Product(6, "Cupple", "Cat01", "Sand", 2),
+        new Product(7, "Soy Soy", "Cat04", "Sand", 2)
     ];
 
     getProducts() : Observable<Product[]> {
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,23 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
-  private selectedCategory : string;
+  public selectedCategory = null;
+  public productPerPage = 4;
+  public selectedPage = 1;
   
   constructor(private repository: ProductRepository) { }
 
   ngOnInit() {
   }
 
+  // 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productPerPage;
+    return this.repository.getProducts(this.selectedCategory)
+    .slice(pageIndex, pageIndex + this.productPerPage);
   }
 
+  // 
   get categories(): string[] {
     return this.repository.getCategories();
   }
 
+  // 
   changeCategory (newCategory? : string) {
     this.selectedCategory = newCategory;
   }
 
+  // 
+  changePage(newPage : number) {
+    this.selectedPage = newPage
+  }
+
+  // 
+  changePageSize(newSize : number) {
+    this.productPerPage = Number(newSize);
+    this.changeCategory(1);
+  }
+
+  // 
+  get pageNumbers() : Number[] {
+    return Array(Math.ceil(this.repository
+        .getProducts(this.selectedCategory).length / this.productPerPage))
+        .fill(0).map((x,i) => i+1);
+  }
 }
