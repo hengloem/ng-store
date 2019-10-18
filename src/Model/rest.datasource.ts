6f89d2211913d381351584f4026f18842c31cf44
hengloem <heng.loem2017@gmail.com>
2019-10-18 09:17:01 +0700

    Generate json fake data

--- src/Model/rest.datasource.ts
+++ src/Model/rest.datasource.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 import { map } from "rxjs/operators";
 
 const PROTOCOL = "http";
-const PORT = 3500;
+const PORT = 4210;
 
 @Injectable()
 export class RestDataSource {
@@ -15,10 +15,10 @@ export class RestDataSource {
     auth_token: string;
 
     constructor(private http: HttpClient) {
-        this.baseUrl = '${PROTOCOL}://${location.hostname}:${PORT}/';
+        this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
     }
 
-    getProduct(): Observable <Product[]> {
+    getProducts(): Observable <Product[]> {
         return this.http.get<Product[]>(this.baseUrl + "products");
     }
 
